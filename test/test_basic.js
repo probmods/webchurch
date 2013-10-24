a8f20d52a62f527f89e63d22df5d0cf87654d31e
Long Ouyang <longouyang@gmail.com>
2013-10-24 10:40:21 -0700

    Start importing some tests from probmods chapters

--- test/test_basic.js
+++ test/test_basic.js
@@ -1,7 +1,22 @@
+/* global console, require */
+
+var log = function() {
+  var array = Array.prototype.slice.call(arguments, 0);
+  array.forEach(function(x) {
+    console.log(x);
+  }); 
+};
+
+// make a string appear red in terminal
+// HT http://stackoverflow.com/a/17524301/351392
+var red = function(str) {
+  return "\033[31m" + str + ":\033[91m";
+};
+
 var evaluate = require("../evaluate.js").evaluate;
 var church_builtins = require("../church_builtins.js");
 
-tests = [
+var tests = [
 	["()", "()"],
 
 	["(+ 1 1)", 2],
@@ -35,25 +50,41 @@ tests = [
 
 	["(cond (#t 1))", 1],
 	["(cond (#f 1) (#t 2))", 2],
-	["(cond (#f 1) (#f 2) (else 3))", 3]
-]
+	["(cond (#f 1) (#f 2) (else 3))", 3],
+  // variadic
+  ["(define (f . x) x) (f 1 2 3)", "(1 2 3)"],
+  ["(apply + (repeat 123 (lambda () (apply + (repeat 1000 (lambda () 1))))))", 123000],
+  ["(map (lambda (x y) (* x y)) '(1 2 3 4) '(5 6 7 8 9))", "(5 12 21 32)"],
+  // flip a coin 1000 times, make sure it comes up heads fewer than 600
+  ["(< (apply + (map (lambda (x) (if x 1 0)) (repeat 1000 flip))) 600)", "#t"],
+  ["(> (* (gaussian 0 1) (gaussian 0 1) ) -1000)", "#t"],
+  ["(define f (mem (lambda (x) (gaussian 0 1)))) (= (f 'a) (f 'a))", "#t"],
+  ["(define make-coin (lambda (weight) (lambda () (flip weight)))) (define coin (make-coin 0.8)) (define data (repeat 1000 (lambda () (sum (map (lambda (x) (if x 1 0)) (repeat 10 coin)))))) (> (mean data) 0.79)", "#t"],
+  ["(define (f) (define lung-cancer (flip 0.01)) (define cold (flip 0.2)) (define cough (or cold lung-cancer)) (if cough 1 0)) (> (apply + (repeat 10000 f)) 2000)", "#t"]
+  
+];
 
 for (var i = 0; i < tests.length; i++) {
-	var result;
+	var churchCode = tests[i][0],
+      expectedResult = tests[i][1],
+      result;
 	
 	try {
 		result = evaluate(tests[i][0]);
 	} catch(err) {
-		result = err.message
+		result = err.message;
 	}
-	if (result != tests[i][1]) {
-		console.log("Failed:\n" +
-					tests[i][0] +
-					"\n\nGot:\n" +
-					result +
-					"\n\nExpected:\n" +
-					tests[i][1] +
-					"\n\n******\n");
-	}
-	
+	if (result != expectedResult) {
+    
+
+		log(red("Code"),
+        churchCode,
+        "",
+        red("Got"),
+        expectedResult,
+        "", 
+        red("Expected"),
+        result,
+        "");
+	} 
 }
